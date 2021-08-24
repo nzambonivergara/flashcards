51a2f5500304bf42de4bb4c7c8fc057a183c0be2
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-08-23 20:53:11 -0600

    Remove unnecessary beforeEach block

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -5,17 +5,13 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
 describe('Deck', () => {
-  let deck;
-  let cards;
 
-  beforeEach(() => {
-    cards = [
+  let cards = [
       new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter'),
       new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder'),
       new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald')
     ]
-    deck = new Deck(cards);
-  })
+  let deck = new Deck(cards);
 
   it('should be a function', () => {
     expect(Deck).to.be.a('function');

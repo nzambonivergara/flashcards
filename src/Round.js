17d93ec6a7337508a8a98122f73e073cc07454b7
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-08-24 21:22:22 -0600

    Change currentCards assignment

--- src/Round.js
+++ src/Round.js
@@ -4,7 +4,7 @@ class Round {
   constructor(deck) {
     this.deck = deck.cards;
     this.turns = 0;
-    this.currentCard = this.deck[this.turns];
+    this.currentCard = this.deck[0];
     this.incorrectGuesses = [];
     this.correctGuesses = []
   }

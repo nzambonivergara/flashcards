7b035490699c0b671ac504fb273b22ef15534f46
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-08-24 21:21:36 -0600

    Add constructor properties and start method

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,16 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.cards = [];
+    this.deck = {};
+    this.currentRound = {};
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +19,20 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    this.cards = prototypeQuestions.map(card => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer)
+    });
+
+    this.deck = new Deck(this.cards);
+
+    this.currentRound = new Round(this.deck);
+
+    this.printMessage(this.deck, this.currentRound);
+
+    this.printQuestion(this.currentRound);
   }
 }
 
